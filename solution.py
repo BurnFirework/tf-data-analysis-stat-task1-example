9247239bc7e825778a8ab2d7e0586437d897e149
BurnFirework <99072007+BurnFirework@users.noreply.github.com>
2023-04-04 20:04:20 -0700

    first solution

--- solution.py
+++ solution.py
@@ -5,7 +5,6 @@ import numpy as np
 chat_id = 860138765
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    t = 66.0
+    l = np.sum(x) / (t * len(x))
+    return l
